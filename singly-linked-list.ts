6561fdba2307d612b23638176e8fed07a3209ad4
siegfriedbz <siegfried.bozza@yahoo.com>
2024-07-10 18:59:02 +0800

    add pop method

--- singly-linked-list.ts
+++ singly-linked-list.ts
@@ -67,6 +67,36 @@ class SinglyLinkedList {
     return this
   }
 
+  pop() {
+    // Return null if list is empty or has no nodes
+    if (!this._head || this._length < 1) return
+
+    // get current head
+    let currentNode = this._head as TListNode
+
+    // case length === 1
+    if (this._length === 1) {
+      this._head = null
+      this._tail = null
+      this._length = 0
+      return currentNode // previous head
+    }
+
+    // else: traverse list & find node previous to tail
+    let prevNode: TListNode | null = null // Track the node just before currentNode
+    while (currentNode.next !== null) {
+      prevNode = currentNode
+      currentNode = currentNode.next as TListNode
+    }
+
+    // move current tail to prevNode
+    const previousTail = this._tail
+    this._tail = prevNode
+    this._length--
+
+    return previousTail
+  }
+
   // class methods
 }
 
@@ -77,7 +107,4 @@ const nodeC = new ListNode('C')
 const newList = new SinglyLinkedList()
 newList.push(nodeA).push(nodeB).push(nodeC)
 
-console.log(newList.length)
-console.log(newList.head)
-console.log(newList.head?.next)
-console.log(newList.head?.next?.next)
+newList.pop()
